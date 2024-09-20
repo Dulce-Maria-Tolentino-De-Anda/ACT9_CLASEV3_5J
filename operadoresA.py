62ea7d187a402da90d7be4545f771beb4289149c
Dulce-Maria-Tolentino-De-Anda <a.22308051281367@cbtis128.edu.mx>
2024-09-20 10:15:27 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,58 @@
+print("act 9  clases v2")
+print("Dulce Tolentino Mat: 22308051281367")
+# zona de class
+class Operadores1367:
+    # zona de funiones
+    def suma1367(self,D,T):
+        s1367=D+T
+        print(f"la suma de {D} + {T} = {s1367}")
+        print("\n")
+    def resta1367(self,D,T):
+        s1367=D-T
+        print(f"la resta de {D} - {T} = {s1367}")
+        print("\n")
+    def multi1367(self,D,T):
+        s1367=D*T
+        print(f"la multiplicacion de {D} * {T} = {s1367}")
+        print("\n")
+    def div1367(self,D,T):
+        s1367=D/T
+        print(f"la division de {D} / {T} = {s1367}")
+        print("\n")
+    def modulo1367(self,D,T):
+        s1367=D%T
+        print(f"el modulo de {D} % {T} = {s1367}")
+        print("\n")
+    def expo1367(self,D,T):
+        s1367=D**T
+        print(f"la exponente de {D} ** {T} = {s1367}")
+        print("\n")
+    def cos1367(self,D,T):
+        s1367=D/T
+        print(f"la cociente de {D} // {T} = {s1367}")
+        print("\n")
+        
+# zona de creacion del objeto
+operaDulce=Operadores1367()
+
+# zona de uso de objetos
+print(" Funcion suma")
+operaDulce.suma1367(5,4)
+
+print(" Funcion resta")
+operaDulce.resta1367(9,3)
+
+print(" Funcion multiplicacion")
+operaDulce.multi1367(6,9)
+
+print(" Funcion division")
+operaDulce.div1367(10,4)
+
+print(" Funcion modulo")
+operaDulce.modulo1367(43,23)
+
+print(" Funcion exponente")
+operaDulce.expo1367(14,9)
+
+print(" Funcion cociente")
+operaDulce.cos1367(3,2)
